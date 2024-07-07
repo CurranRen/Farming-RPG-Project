9e6d1610b6f373dec6f6709c2a85e9281153b8db
Ren <renzikun0903@126.com>
2024-07-07 20:14:21 +0800

    Create Player Class and Player Movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SingletonMonobehaviour<Player>
+{
+
+    // 移动参数
+    private float xInput;
+
+    private float yInput;
+    private bool isCarrying = false;
+    private bool isIdle;
+    private bool isLiftingToolDown;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolUp;
+    private bool isRunning;
+    private bool isUsingToolDown;
+    private bool isUsingToolLeft;
+    private bool isUsingToolRight;
+    private bool isUsingToolUp;
+    private bool isSwingingToolDown;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolUp;
+    private bool isWalking;
+    private bool isPickingUp;
+    private bool isPickingDown;
+    private bool isPickingLeft;
+    private bool isPickingRight;
+    private ToolEffect toolEffect = ToolEffect.none;
+
+    private Rigidbody2D rigidBody2D;
+
+    private Direction playerDirection;
+
+    private float movementSpeed;
+
+    private bool _playerInputIsDisabled = false;
+    public bool PlayerInputIsDisabled { get => _playerInputIsDisabled; set => _playerInputIsDisabled = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        #region Player Input
+
+        if (!PlayerInputIsDisabled)
+        {
+            ResetAnimationTriggers();
+
+            PlayerMovementInput();
+
+            PlayerWalkInput();
+
+
+            // 发送事件给监听玩家移动输入的监听器
+            EventHandler.CallMovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying, toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                false, false, false, false);
+        }
+
+        #endregion Player Input
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    // 根据输入移动玩家刚体的位置
+    private void PlayerMovement()
+    {
+        Vector2 move = new Vector2(xInput * movementSpeed * Time.fixedDeltaTime, yInput * movementSpeed * Time.fixedDeltaTime);
+
+        rigidBody2D.MovePosition(rigidBody2D.position + move);
+    }
+
+    private void ResetAnimationTriggers()
+    {
+        isPickingRight = false;
+        isPickingLeft = false;
+        isPickingUp = false;
+        isPickingDown = false;
+        isUsingToolRight = false;
+        isUsingToolLeft = false;
+        isUsingToolUp = false;
+        isUsingToolDown = false;
+        isLiftingToolRight = false;
+        isLiftingToolLeft = false;
+        isLiftingToolUp = false;
+        isLiftingToolDown = false;
+        isSwingingToolRight = false;
+        isSwingingToolLeft = false;
+        isSwingingToolUp = false;
+        isSwingingToolDown = false;
+        toolEffect = ToolEffect.none;
+    }
+
+    // 捕获玩家移动的输入并基于输入设置移动参数
+    private void PlayerMovementInput()
+    {
+        yInput = Input.GetAxisRaw("Vertical");
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        // 沿斜向移动1个单位（1/根号2 ≈ 0.71）
+        if (yInput != 0 && xInput != 0)
+        {
+            xInput = xInput * 0.71f;
+            yInput = yInput * 0.71f;
+        }
+
+        if (xInput != 0 || yInput != 0)
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+
+            // 捕获玩家方向用于SL时恢复玩家方向
+            if (xInput < 0)
+            {
+                playerDirection = Direction.left;
+            }
+            else if (xInput > 0)
+            {
+                playerDirection = Direction.right;
+            }
+            else if (yInput < 0)
+            {
+                playerDirection = Direction.down;
+            }
+            else
+            {
+                playerDirection = Direction.up;
+            }
+        }
+        else if (xInput == 0 && yInput == 0)
+        {
+            isRunning = false;
+            isWalking = false;
+            isIdle = true;
+        }
+    }
+
+    private void PlayerWalkInput()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isRunning = false;
+            isWalking = true;
+            isIdle = false;
+            movementSpeed = Settings.walkingSpeed;
+        }
+        else
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+        }
+    }
+}
